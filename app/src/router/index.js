74698b72b3153f222b39557be09d33bc72d59297
jnthhn <jonathanh.ung987@gmail.com>
2025-04-09 12:27:35 -0400

    fg

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Schedules from '../views/Schedules.vue'
+import Schedules from '../components/Schedules.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Schedule />
+  </div>
+</template>
+
+<script>
+import Schedule from './components/Schedule.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Schedule,
+  },
+}
+</script>
