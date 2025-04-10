c7ea86290ec30ec182e373166a1baea1fd2e17af
YaolinTan <yaolint@nycstudents.net>
2025-04-10 12:30:54 -0400

    e

--- app/src/components/SignIn.vue
+++ app/src/components/SignIn.vue
@@ -9,12 +9,17 @@
       <input v-model="password" type="password" required />
     </div>
     <button type="submit">Login</button>
+    <button @click="handleLogout">Log Out</button>
+    <p v-if="auth.isLoggedin">Lebonbon</p>
   </form>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const username = ref('')
 const password = ref('')
@@ -24,9 +29,20 @@ const auth = useAuthStore()
 function handleLogin() {
   if (username.value === 'admin' && password.value === 'password') {
     auth.login()
-    alert('Login successful!')
+    successfulLogin()
   } else {
     alert('Invalid credentials!')
   }
+  username.value = ''
+  password.value = ''
+}
+
+function handleLogout() {
+  auth.logout()
+  alert('Logout sucessful')
+}
+
+function successfulLogin() {
+  router.push('/about')
 }
 </script>
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button>Logout</button>
   </div>
 </template>
+
+<script setup>
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
