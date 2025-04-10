fe7ef58f99fd0385d8be77e57a57d61a59e4be9b
jnthhn <jonathanh.ung987@gmail.com>
2025-04-10 11:54:01 -0400

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,16 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <div id="app">
+    <Schedule />
+  </div>
 </template>
+
+<script>
+import Schedule from './components/Schedules.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Schedule,
+  },
+}
+</script>
