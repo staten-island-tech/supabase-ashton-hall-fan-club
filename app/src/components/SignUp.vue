9241c112dbc3dc54207d6b2235bc4cf117fcfb49
YaolinTan <yaolint@nycstudents.net>
2025-04-21 12:30:37 -0400

    e

---
+++ app/src/components/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSignUp">
+      <div>
+        <label>Username:</label>
+        <input v-model="email" type="text" required />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input v-model="password" type="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
